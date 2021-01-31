fa7a8ce163a28834c0eeeefc231ad609cbeaacda
Diego Bonetto <diego_yyz@hotmail.com>
2021-01-31 19:29:53 -0300

    navmesh zones added and new classes boat

--- Assets/Scripts/Entities/EnemyBoat.cs
+++ Assets/Scripts/Entities/EnemyBoat.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBoat : MonoBehaviour
+public class EnemyBoat : Enemy
 {
     [SerializeField]
     private Transform[] Waypoints;
 
 
+    Vector3 returnRandomPoint()
+    {
+        int D = Random.Range(0, Waypoints.Length);
+        return Waypoints[D].position;
+    }
+    private void Start()
+    {
+        agent.SetDestination(returnRandomPoint());
+    }
+    public new void Update()
+    {
+        if (agent.remainingDistance < 0.5)
+        {
+            agent.SetDestination(returnRandomPoint());
+        }
 
+    }
 }
